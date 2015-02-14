2aace574186bd37ad0af9970e0bb9342ef5be62f
Lauricio Su <me@lauricio.com>
2015-02-14 14:46:14 +0700

    initial commit

---
+++ package.js
@@ -0,0 +1,31 @@
+Package.describe({
+  name: 'ox2:sortable',
+  summary: 'Sortable list components',
+  version: '1.0.0',
+  git: ' /* Fill me in! */ '
+});
+
+var S = 'server';
+var C = 'client';
+var CS = [C, S];
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.2.1');
+  // Core
+  api.use([
+    'templating'
+    ]);
+  // 3rd party
+  api.use([
+    'lauricio:less-autoprefixer@1.0.15','mquandalle:jade@0.4.1'
+    ]);
+  api.addFiles('lib/oo-banner.jade', C);
+  api.addFiles('lib/oo-banner.js', C);
+  api.addFiles('lib/oo-banner.less', C);
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('ox2:banner');
+  api.addFiles('tests/oo-banner-tests.js');
+});
