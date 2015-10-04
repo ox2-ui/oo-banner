b601c95e5701d61b65feaae9a7109970688c8319
Lauricio Su <me@lauricio.com>
2015-10-04 19:30:23 +0700

    updated less dependency to 2.5.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ox2:banner',
   summary: 'TESTING_DO_NOT_USE Banner component',
-  version: '1.0.1',
+  version: '1.1.1',
   git: ' /* Fill me in! */ '
 });
 
@@ -10,14 +10,14 @@ var C = 'client';
 var CS = [C, S];
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.2.1');
+  api.versionsFrom('1.2.0.2');
   // Core
   api.use([
     'templating'
     ]);
   // 3rd party
   api.use([
-    'lauricio:less-autoprefixer@1.0.15','mquandalle:jade@0.4.1','meteorhacks:subs-manager@1.3.0'
+    'lauricio:less-autoprefixer@2.5.0_3','mquandalle:jade@0.4.1','meteorhacks:subs-manager@1.3.0'
     ]);
   api.addFiles('lib/oo-banner.jade', C);
   api.addFiles('lib/oo-banner.js', C);
